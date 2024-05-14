13e73e5ae49b90052947a90a1b5a0463ea3dacef
Andreas Pehrsson <apehrsson@hotmail.com>
2024-05-14 14:38:57 +0200

    testing testing

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -1,9 +1,10 @@
 import React from 'react';
 import { render, screen } from '@testing-library/react';
 import TodosPage from './pages/todos';
+import { BrowserRouter } from 'react-router-dom';
 
 test('renders learn react link', () => {
-  render(<TodosPage />);
+  render(<BrowserRouter><TodosPage /></BrowserRouter>);
   const linkElement = screen.getByText(/My Todos/i);
   expect(linkElement).toBeInTheDocument();
 });
