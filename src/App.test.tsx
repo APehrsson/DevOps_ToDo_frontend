a3e5f5bae1df291ed0a19143ffbe9b1558ad8227
Andreas Pehrsson <apehrsson@hotmail.com>
2024-05-20 13:53:55 +0200

    uppdaterat till appV2

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -1,10 +1,35 @@
 import React from 'react';
-import { render, screen } from '@testing-library/react';
-import TodosPage from './pages/todos';
+import { render } from '@testing-library/react';
+import AuthPage from './pages/auth';
 import { BrowserRouter } from 'react-router-dom';
+import TodosPage from './pages/todos';
 
-test('renders learn react link', () => {
-  render(<BrowserRouter><TodosPage /></BrowserRouter>);
-  const linkElement = screen.getByText(/My Todos/i);
-  expect(linkElement).toBeInTheDocument();
+describe('AuthPage component', () => {
+  it('renders AuthPage component correctly', () => {
+    const { getAllByText } = render(
+      <BrowserRouter>
+        <AuthPage />
+      </ BrowserRouter>
+    );
+
+    // Verificar si el título "My Todos" está presente en la página renderizada
+    expect(getAllByText('Login')).toHaveLength(2);
+    expect(getAllByText('Register')).toHaveLength(1);
+  });
+});
+
+describe('TodosPage component', () => {
+  it('renders TodosPage component correctly', () => {
+    const { getByText } = render(
+      <BrowserRouter>
+        <TodosPage />
+      </BrowserRouter>
+    );
+
+    // Verificar si el título "My Todos" está presente en la página renderizada
+    expect(getByText('My Todos')).toBeInTheDocument();
+
+    // Verificar si el botón de logout está presente en la página renderizada
+    expect(getByText('Logout')).toBeInTheDocument();
+  });
 });
